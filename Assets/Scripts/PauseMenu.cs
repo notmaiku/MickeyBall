5cfa1959151a98edadf0b59b79ca514791005a93
Jay <jbruce3@masonlive.gmu.edu>
2018-07-30 01:53:40 -0400

    Menu
    Created main and pause menus

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    private void Start()
+    {
+        Resume();
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+	}
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadScene(string name)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
